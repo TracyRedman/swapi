399c2855035f9223520df661759128d8655221c6
Tracy Redman <redmanatracy@gmail.com>
2022-07-06 20:55:27 -0400

    SwapiFin

--- SwapiMVC/Controllers/PeopleController.cs
+++ SwapiMVC/Controllers/PeopleController.cs
@@ -1,18 +1,36 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 public class PeopleController : Controller
+{
+    private readonly HttpClient _httpClient;
+    public PeopleController(IHttpClientFactory httpClientFactory)
     {
-        private readonly HttpClient _httpClient;
-        public PeopleController(IHttpClientFactory httpClientFactory)
-        {
-
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _httpClient=httpClientFactory.CreateClient("swapi");
     }
+    public async Task<IActionResult> Index(string page)
+    {
+        string route = $"people?page={page ?? "1"}";
+        HttpResponseMessage response = await _httpClient.GetAsync(route);
+
+        
+        var people = await response.Content.ReadAsAsync<ResultsViewModel<PeopleViewModel>>();
+        return View(people);
+    }
+
+    public async Task<IActionResult> Person(string id)
+    {
+        var response = await _httpClient.GetAsync($"people/{id}");
+        if (id is null || response.IsSuccessStatusCode == false)
+            return RedirectToAction(nameof(Index));
+    
+    var responseString = await response.Content.ReadAsStringAsync();
+    var person = JsonSerializer.Deserialize<PeopleViewModel>(responseString);
+    return View(person);
+    }
+
+}
